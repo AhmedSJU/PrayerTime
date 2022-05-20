d60bb1656e1f01d9ead8124d4567197cc8fad097
Sheikh Ahmed <sheikh@jerin.app>
2022-05-20 14:57:47 +0100

    initial project

---
+++ PrayerTime/PrayerTime/PrayerTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrayerTimeApp.swift
+//  PrayerTime
+//
+//  Created by Sheikh Ahmed on 20/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct PrayerTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
